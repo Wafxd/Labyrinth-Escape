efe500fdf9f8babd1add4e43dfed10eee2fdbfc0
Faris Wafda <farisjaguar2@gmail.com>
2024-11-06 21:25:32 +0700

    ubah semua

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public int speed = 10;
+    public int speed = 2;
     private Rigidbody2D characterBody;
     private Vector2 velocity;
     private Vector2 inputMovement;
